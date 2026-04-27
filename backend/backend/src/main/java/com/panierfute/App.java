94f928cb2c66c23ff22d6f55cf7218f2edf10ba0
Robin Sanneh <sanneh.robin.m@gmail.com>
2026-04-27 15:35:20 +0200

    chore: init Angular frontend + Java backend

---
+++ backend/backend/src/main/java/com/panierfute/App.java
@@ -0,0 +1,13 @@
+package com.panierfute;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
